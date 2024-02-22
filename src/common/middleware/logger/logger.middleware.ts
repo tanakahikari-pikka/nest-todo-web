b6b5b281c4748eb1e80ab5d9e7d33af257327e5f
tanakahikari-pikka <hikaricareer1999@gmail.com>
2024-02-22 18:49:21 +0900

    ログの出力時の日時追加

--- src/common/middleware/logger/logger.middleware.ts
+++ src/common/middleware/logger/logger.middleware.ts
@@ -1,11 +1,17 @@
 export function httpObserveLogger(req, res, next) {
 	if (req != null) {
 		console.log(
-			`HTTP Request method: ${req.method} URL: ${req.path} IP: ${req.host} `,
+			// 現在の時間を出力
+
+			`HTTP Request date: ${new Date()} method: ${req.method} URL: ${
+				req.path
+			} IP: ${req.host} `,
 		);
 	}
 	if (res != null) {
-		console.log(`HTTP Response status code: ${res.statusCode}`);
+		console.log(
+			`HTTP Response status date: ${new Date()}  code: ${res.statusCode}`,
+		);
 	}
 	next();
 }
