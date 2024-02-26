c47440ae05e131f61fd5e680ebfdfc7a455a200f
pikka1999 <107383003+tanakahikari-pikka@users.noreply.github.com>
2024-02-26 17:20:24 +0900

    Update src/todos/todos.service.ts
    Co-authored-by: yuim <66901428+yuichimukai@users.noreply.github.com>

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -21,7 +21,18 @@ export class TodosService {
           }
           return response.json();
         },
-      );
+     return fetch(`[https://jsonplaceholder.typicode.com/todos/${id}](https://jsonplaceholder.typicode.com/todos/$%7Bid%7D)`)
+    .then(response => {
+      if (response.status === 404) {
+        throw new NotFoundException("データが見つかりませんでした");
+      } else if (!response.ok) {
+        throw new Error("データの取得に失敗しました: " + response.status);
+      }
+      return response.json();
+    })
+    .catch((_e) => {
+      throw new Error("データの取得に失敗しました: " );
+    });
     } catch (e) {
       throw new Error("データの取得に失敗しました: ");
     }
