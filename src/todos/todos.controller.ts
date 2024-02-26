e1e230034cbba5d0e461bf704070ab4ee74ca8d5
tanakahikari-pikka <hikaricareer1999@gmail.com>
2024-02-26 13:22:29 +0900

    postメソッド

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get } from "@nestjs/common";
 import { TodosService } from "./todos.service";
 
 @Controller("todos")
 export class TodosController {
-	constructor(private readonly todosService: TodosService) {}
+  constructor(private readonly todosService: TodosService) {}
 
-	@Get()
-	async fetchTodos() {
-		return this.todosService.fetchTodos();
-	}
+  @Get()
+  async fetchTodos() {
+    return this.todosService.fetchTodos();
+  }
+
+  @Delete(":id")
+  async deleteTodo() {
+    return this.todosService.deleteTodo(1);
+  }
 }
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -2,14 +2,25 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class TodosService {
-	fetchTodos() {
-		try {
-			// https://jsonplaceholder.typicode.com/todos からデータを取得して返す
-			return fetch("https://jsonplaceholder.typicode.com/todos").then(
-				(response) => response.json(),
-			);
-		} catch (e) {
-			throw new Error("データの取得に失敗しました: ");
-		}
-	}
+  fetchTodos() {
+    try {
+      // https://jsonplaceholder.typicode.com/todos からデータを取得して返す
+      return fetch("https://jsonplaceholder.typicode.com/todos").then(
+        (response) => response.json(),
+      );
+    } catch (e) {
+      throw new Error("データの取得に失敗しました: ");
+    }
+  }
+
+  deleteTodo(id: number) {
+    try {
+      // https://jsonplaceholder.typicode.com/todos からデータを削除して返す
+      return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: "DELETE",
+      }).then((response) => response.json());
+    } catch (e) {
+      throw new Error("データの削除に失敗しました: ");
+    }
+  }
 }
